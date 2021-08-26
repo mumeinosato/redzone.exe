4867578c904efd90667161b95fc58c9823a8b196
github <github>
2021-08-26 13:26:10 +0900

    変更

--- redzone.bat
+++ redzone.bat
@@ -1,2 +1,15 @@
 @echo off
 cscript redzone.vbs
+set WALLPAPER_IMG=
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d %WALLPAPER_IMG% /f
+reg add "HKCU\control panel\desktop" /v WallPaperStyle /t REG_SZ /d 2 /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
--- redzone.vbs
+++ redzone.vbs
@@ -1,1 +1,4 @@
 msgbox "このPCはRED_ZONE.exeにより占拠されました。"& vbCr &"おとなしく餌食になりなさい。",,"RED_ZONE.exeより"
+'For a = 1 To 12    
+    'msgBox "クラシックいいよね～",vbExclamation,"クラシック"
+'Next 
